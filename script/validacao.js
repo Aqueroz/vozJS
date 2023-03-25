1c967a00972d1fd5bc1769cbe8e2be80a126e10c
Caio <73550623+Aqueroz@users.noreply.github.com>
2023-03-25 01:04:08 -0300

    Update validacao.js

--- script/validacao.js
+++ script/validacao.js
@@ -20,12 +20,12 @@ function verificaSeOChutePossuiUmValorValido(chute) {
 
     else if (numero > numeroSecreto){
         elementoChute.innerHTML += `
-        <div>O número secreto é menor <i class="fa-solid fa-up-long"></i> </div>
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i> </div>
         `
     }
     else{
         elementoChute.innerHTML += `
-        <div>O número secreto é maior <i class="fa-solid fa-down-long"></i> </div>
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i> </div>
         `
     }
 
